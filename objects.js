2cf34926f0e007eaf58e69412534b20d6c1a9369
casey bradford <casey@flatironschool.com>
2019-02-09 17:46:56 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,13 @@
 var playlist = { artist: "title"};
 
 function updatePlaylist(obj, key, value){
-  obj.key = value;
+  obj[key] = value
+  
+  return obj
+}
+
+function removeFromPlaylist(obj, key){
+  
+  
+  return obj;
 }
